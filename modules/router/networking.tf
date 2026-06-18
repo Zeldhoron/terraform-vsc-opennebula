b11d25e8b95be544738d8d7be7bb92f8f535d6b8
Anthony Beerens <anthony.beerens@gmail.com>
2026-06-18 14:49:14 +0200

    Fix waldur group name extraction

--- modules/router/networking.tf
+++ modules/router/networking.tf
@@ -5,7 +5,7 @@ locals {
   public_network_suffix = var.vsc ? "vsc" : "public"
 
   public_net = var.use_demo_format ? "${local.group}_${local.public_network_suffix}" : "public"
-  vm_net = "${local.group}_${var.use_demo_format ? local.vm_network_suffix : "internal_vsc"}"
+  vm_net = "${local.group}_${var.use_demo_format ? local.vm_network_suffix : "internal"}"
 }
 
 data "opennebula_virtual_network" "external" {
